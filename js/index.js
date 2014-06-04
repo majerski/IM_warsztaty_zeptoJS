61d61dfffb4b10f93e976e6f4d8e5d3e52a10169
majerski <tomasz.majerski@o2.pl>
2014-06-04 12:03:44 +0200

    .

--- js/index.js
+++ js/index.js
@@ -1,8 +1,107 @@
-var warsztaty = [];
-var feedFromServer = false;
-var feedFromLocal = true;
-var warsztaty_loaded = false;
-var fi_path = 'installed.dat';
+var	warsztaty = [],
+	artykuly = [],
+	sources_loaded = false,
+	warsztaty_loaded = false,
+	artykuly_loaded = false,
+	fi_path = 'installed.dat',
+	//artykulyUrl = 'http://www.q-service.com.pl/rss/',
+	artykulyUrl = 'http://arcontact.pl/warsztaty_inter_cars/rss.php',
+	warsztatyUrl = 'http://arcontact.pl/warsztaty_inter_cars/feed.php',
+	form_email = 'mifdetal@intercars.eu',
+	map,
+	startingLatitude = 52.069347,
+	startingLongitude = 19.480204;
+
+function checkConnection() {
+    if(typeof navigator.connection == 'undefined' || typeof navigator.connection.type == 'undefined') {
+		return 'No network connection';
+	}
+	var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+    return states[networkState];
+}
+function gotConnection(){
+	//
+	return true;
+	//
+	var checkConnection = checkConnection();
+	if(checkConnection == 'No network connection'){return false;}
+	return true;
+}
+function feedArtykuly(){
+	if(gotConnection()){
+		$.ajax({
+			url: artykulyUrl,
+			type: 'GET',
+			async: false,
+			cache: false,
+			success: function(response){
+				if(typeof response != 'undefined'){
+					artykuly = response;
+					artykuly_loaded = true;
+				} else {
+					artykuly_loaded = false;
+				}
+			},
+			error: function(){
+				artykuly_loaded = false;
+			}
+		});
+	} else {
+		if(typeof window.plugins != 'undefined' && typeof window.plugins.toast != 'undefined'){
+			window.plugins.toast.showLongCenter('Brak połączenia z internetem.',function(a){},function(b){});
+		}
+		artykuly_loaded = false;
+	}
+}
+function renderArtykuly(){
+	$("#artykuly").html('<div class="panel callout radius">załadowano artykuły</div>');
+}
+$(document).ready(function() {
+	$("header .logo").on("click",function(){
+		$("header ul li a").removeClass("active");
+	});
+	$(document).on("pagebeforechange",function(e,eventData){
+		$("header ul li a").removeClass("active");
+		var targetID = eventData.toPage;
+		$('header ul li a[href="'+targetID+'"]').addClass("active");
+	});
+	$(".loader").fadeOut();
+	$("footer").animate({
+		"bottom":0
+	},{
+		duration:300,
+		easing:"ease"
+	});
+	if(!sources_loaded){
+		if(gotConnection()){
+			feedArtykuly();
+		} else {
+			artykuly_loaded = false;
+		}
+	}
+	if(artykuly_loaded && warsztaty_loaded){
+		sources_loaded = true;
+	}
+	if(artykuly_loaded){
+		renderArtykuly();
+	} else {
+		var artykulyDiv = document.getElementById("artykuly");
+		if(gotConnection()) {
+			artykulyDiv.innerHTML = '<div class="panel callout radius">Nie udało się wgrać aktualności.</div>';
+		} else {
+			artykulyDiv.innerHTML = '<div class="panel callout radius">Włącz internet aby pobrać najnowsze aktualności.</div>';
+		}
+	}
+});	
 
 var app = {
     initialize: function() {
@@ -39,35 +138,24 @@ var app = {
 			});
 		}, failFS); 
 		function failFS(){
-			// fail FS
+			window.plugins.Shortcut.CreateShortcut("Inter Cars",function(a){},function(b){});
 		}
-		$(document).ready(function() {
-			$("header .logo").on("click",function(){
-				$("header ul li a").removeClass("active");
-			});
-			$(document).on("pagebeforechange",function(e,eventData){
-				$("header ul li a").removeClass("active");
-				var targetID = eventData.toPage;
-				$('header ul li a[href="'+targetID+'"]').addClass("active");
-			});
-			$(".loader").fadeOut();
-		});
+		// tutaj skrypt
     },
 	onLoad: function() {
 		
     },
 	onOffline: function() {
-		window.plugins.toast.showLongCenter('Brak połączenia z internetem.',function(a){},function(b){});
-		if(!warsztaty_loaded) {
-			feedFromServer = false;
-			feedFromLocal = true;
+		if(typeof window.plugins != 'undefined' && typeof window.plugins.toast != 'undefined'){
+			window.plugins.toast.showLongCenter('Brak połączenia z internetem.',function(a){},function(b){});
 		}
 	},
 	onOnline: function() {
-		window.plugins.toast.showShortBottom('Nawiązano połączenie z internetem.',function(a){},function(b){});
-		if(!warsztaty_loaded) {
-			feedFromServer = true;
-			feedFromLocal = false;
+		if(typeof window.plugins != 'undefined' && typeof window.plugins.toast != 'undefined'){
+			window.plugins.toast.showShortBottom('Nawiązano połączenie z internetem.',function(a){},function(b){});
+		}
+		if(!artykuly_loaded){
+			feedArtykuly();
 		}
     }
 };

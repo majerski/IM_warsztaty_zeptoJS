193f3e2bf4f6cbc6c0e3606699bf429dc06b04f3
majerski <tomasz.majerski@o2.pl>
2014-06-04 14:16:45 +0200

    .

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 var	warsztaty = [],
 	artykuly = [],
-	sources_loaded = false,
 	warsztaty_loaded = false,
 	artykuly_loaded = false,
 	fi_path = 'installed.dat',
-	//artykulyUrl = 'http://www.q-service.com.pl/rss/',
-	artykulyUrl = 'http://arcontact.pl/warsztaty_inter_cars/rss.php',
+	artykulyUrl = 'http://www.q-service.com.pl/rss/',
+	//artykulyUrl = 'http://arcontact.pl/warsztaty_inter_cars/rss.php',
 	warsztatyUrl = 'http://arcontact.pl/warsztaty_inter_cars/feed.php',
 	form_email = 'mifdetal@intercars.eu',
 	map,
@@ -13,8 +12,11 @@ var	warsztaty = [],
 	startingLongitude = 19.480204;
 
 function checkConnection() {
-    if(typeof navigator.connection == 'undefined' || typeof navigator.connection.type == 'undefined') {
-		return 'No network connection';
+	//
+	return 'ok';
+	//
+	if(typeof navigator.connection == 'undefined' || typeof navigator.connection.type == 'undefined') {
+		return 'fail';
 	}
 	var networkState = navigator.connection.type;
     var states = {};
@@ -25,12 +27,12 @@ function checkConnection() {
     states[Connection.CELL_3G]  = 'Cell 3G connection';
     states[Connection.CELL_4G]  = 'Cell 4G connection';
     states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+    states[Connection.NONE]     = 'fail';
     return states[networkState];
 }
 function gotConnection(){
 	var a = checkConnection();
-	if(a == 'No network connection'){return false;}
+	if(a == 'fail'){return false;}
 	return true;
 }
 function feedArtykuly(){
@@ -40,6 +42,7 @@ function feedArtykuly(){
 			type: 'GET',
 			async: false,
 			cache: false,
+			dataType: 'xml',
 			success: function(response){
 				if(typeof response != 'undefined'){
 					artykuly = response;
@@ -60,7 +63,21 @@ function feedArtykuly(){
 	}
 }
 function renderArtykuly(){
-	$("#artykuly").html('<div class="panel radius">załadowano artykuły</div>');
+	var list = document.createElement('ul');
+	var	xmlDoc = $(artykuly).children();
+	var c = $(xmlDoc).find('item');
+	var months = Array("Stycznia", "Lutego", "Marca", "Kwietnia", "Maja", "Czerwca", "Lipca", "Sierpnia", "Września", "Października", "Listopada", "Grudnia");
+	for(var i=0;i<c.length;i++){
+		var title = $($(c[i])[0]).find('title').html();
+		var link = $($(c[i])[0]).find('link').html();
+		var pubDate = $($(c[i])[0]).find('pubDate').html();
+		var _date = new Date(Date.parse(pubDate));
+		var date_string = _date.getDate() + " " + months[_date.getMonth()] + " " + _date.getFullYear();
+		var li = document.createElement('li');
+		li.innerHTML = '<a onclick="window.open(\''+link+'\',\'_system\',\'location=no\')"><i class="fa fa-chevron-circle-right pull-right"></i><h6>'+title+'</h6><span>'+date_string+'</span></a>';
+		list.appendChild(li);
+	}
+	$("#artykuly").html(list);
 }
 $(document).ready(function() {
 	$("header .logo").on("click",function(){
@@ -73,16 +90,13 @@ $(document).ready(function() {
 	});
 	$(".loader").animate({"opacity":0},500,function(){this.remove();});
 	$("footer").animate({"bottom":0},500);
-	if(!sources_loaded){
-		if(gotConnection()){
-			feedArtykuly();
-		} else {
-			artykuly_loaded = false;
-		}
+	
+	if(gotConnection()){
+		feedArtykuly();
+	} else {
+		artykuly_loaded = false;
 	}
-	if(artykuly_loaded && warsztaty_loaded){
-		sources_loaded = true;
-	}
+	
 	if(artykuly_loaded){
 		renderArtykuly();
 	} else {

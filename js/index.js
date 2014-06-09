5916085c399362a0971b194f76d6e8975969e77a
majerski <tomasz.majerski@o2.pl>
2014-06-09 11:41:22 +0200

    .

--- js/index.js
+++ js/index.js
@@ -44,8 +44,6 @@ function checkConnection() {
 }
 function gotConnection(){
 	var a = checkConnection();
-	window.plugins.toast.showLongTop('gotConnection',function(a){},function(b){});
-	
 	if(a == 'fail'){return false;}
 	return true;
 }

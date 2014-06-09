df01c90599fedd94c44a96a048853ba67267c8dd
majerski <tomasz.majerski@o2.pl>
2014-06-09 11:47:14 +0200

    .

--- js/index.js
+++ js/index.js
@@ -232,9 +232,9 @@ $(document).ready(function() {
 			artykulyDiv.innerHTML = '<div class="panel text-center">Włącz internet aby pobrać najnowsze aktualności. <a onclick="location.reload();"><i class="fa fa-refresh"></i> odśwież</a></div>';
 		}
 	}
-	
+	window.plugins.toast.showLongBottom('warsztaty_from_file: '+warsztaty_from_file,function(a){},function(b){});
 	if(warsztaty_from_file){
-		window.plugins.toast.showLongCenter('warsztaty_from_file',function(a){},function(b){});
+		window.plugins.toast.showLongTop('warsztaty_from_file',function(a){},function(b){});
 		/*window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs){
 			fs.root.getFile(warsztaty_path, {create:false}, fileExists, fileNotExists);
 		}, warsztatyFailFS);*/

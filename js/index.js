080c26a99376083b50bc88384ca4ebc16f85ed8d
majerski <tomasz.majerski@o2.pl>
2014-06-10 09:30:46 +0200

    .

--- js/index.js
+++ js/index.js
@@ -182,6 +182,7 @@ var app = {
 			}
 		}
 		function renderWarsztaty(){
+			$("body").prepend('<div class="text-center pagination_outer warsztaty_pagination_outer"><div class="warsztaty_pagination pagination"><a href="#" class="first" data-action="first">&laquo;</a><a href="#" class="previous" data-action="previous">&lsaquo;</a><input type="text" readonly="readonly" data-max-page="60" /><a href="#" class="next" data-action="next">&rsaquo;</a><a href="#" class="last" data-action="last">&raquo;</a></div></div>');
 			warsztatyDiv.innerHTML = '<div class="warsztaty_pagination"></div>';
 		}
 		function warsztatyLoadError(){
